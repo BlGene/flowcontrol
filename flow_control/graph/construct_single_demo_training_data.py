07c50cb59cdb1110a8767cc17c585e3da5b37642
Martin Alexander Buechner <buechner@informatik.uni-freiburg.de>
2022-12-02 13:14:14 +0100

    adds multi-processing training data construction

--- flow_control/graph/construct_single_demo_training_data.py
+++ flow_control/graph/construct_single_demo_training_data.py
@@ -4,6 +4,9 @@ import json
 import logging
 from glob import glob
 from collections import defaultdict
+import ray
+from ray.util.multiprocessing import Pool
+
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,9 +14,8 @@ import torch
 
 import utils
 
-export_dir = "/tmp/flow_dataset_contrastive/"
 
-def create_graph_data(recordings: list, demo_idcs: list):
+def create_single_demo_graph(recordings: list, demo_idx: int):
 
     edges = list()
     edge_time_delta = list()
@@ -22,26 +24,24 @@ def create_graph_data(recordings: list, demo_idcs: list):
     node_idx = 0
     node_idx2frame = defaultdict()
 
-    # loop through all demonstrations
-    for demo_idx in demo_idcs:
-        print("demo_idx: ", demo_idx)
-        # loop through frames
-        curr_demo_node_idcs = list()
-        for frame_idx in range(utils.get_len(recordings[demo_idx])):
-            curr_image = torch.tensor(utils.get_image(recordings[demo_idx], frame_idx)).unsqueeze(0)
+    print("demo_idx: ", demo_idx)
+    # loop through frames
+    curr_demo_node_idcs = list()
+    for frame_idx in range(utils.get_len(recordings[demo_idx])):
+        curr_image = torch.tensor(utils.get_image(recordings[demo_idx], frame_idx)).unsqueeze(0)
 
-            node_idx2frame[node_idx] = (demo_idx, frame_idx)
+        node_idx2frame[node_idx] = (demo_idx, frame_idx)
 
-            curr_demo_node_idcs.append(node_idx)
-            node_feats = np.vstack((node_feats, curr_image))
-            node_idx += 1
+        curr_demo_node_idcs.append(node_idx)
+        node_feats = np.vstack((node_feats, curr_image))
+        node_idx += 1
 
-        # get all combinations of nodes in the current demo
-        for i in range(len(curr_demo_node_idcs)):
-            for j in range(len(curr_demo_node_idcs)):
-                if i != j and j > i:
-                    edges.append((curr_demo_node_idcs[i], curr_demo_node_idcs[j]))
-                    edge_time_delta.append(node_idx2frame[j][1] - node_idx2frame[i][1])
+    # get all combinations of nodes in the current demo
+    for i in range(len(curr_demo_node_idcs)):
+        for j in range(len(curr_demo_node_idcs)):
+            if i != j and j > i:
+                edges.append((curr_demo_node_idcs[i], curr_demo_node_idcs[j]))
+                edge_time_delta.append(node_idx2frame[j][1] - node_idx2frame[i][1])
 
     # reverse node_idx2frame in one line
     demoframe2node_idx = {str(v): k for k, v in node_idx2frame.items()}
@@ -49,18 +49,59 @@ def create_graph_data(recordings: list, demo_idcs: list):
     edges = torch.tensor(edges).long()
     edge_time_delta = torch.tensor(edge_time_delta).long()
 
-    torch.save(node_feats, os.path.join(export_dir, str(demo_idcs)) + '-node-feats.pth')
-    torch.save(edges, os.path.join(export_dir, str(demo_idcs)) + '-edges.pth')
-    torch.save(edge_time_delta, os.path.join(export_dir, str(demo_idcs)) + '-edge-time-delta.pth')
-    print("saved to: ", os.path.join(export_dir, str(demo_idcs)))
+    torch.save(node_feats, os.path.join(export_dir, "seed" + str(demo_idx)) + '-node-feats.pth')
+    torch.save(edges, os.path.join(export_dir, "seed" + str(demo_idx)) + '-edges.pth')
+    torch.save(edge_time_delta, os.path.join(export_dir, "seed" + str(demo_idx)) + '-edge-time-delta.pth')
+    print("saved to: ", os.path.join(export_dir, str(demo_idx)))
 
-    with open(os.path.join(export_dir, str(demo_idcs)) + '_node_idx2frame.json', 'w') as outfile:
+    with open(os.path.join(export_dir, "seed" + str(demo_idx)) + '_node_idx2frame.json', 'w') as outfile:
         json.dump(node_idx2frame, outfile)
 
-    with open(os.path.join(export_dir, str(demo_idcs)) + '_demoframe2node_idx.json', 'w') as outfile:
+    with open(os.path.join(export_dir, "seed" + str(demo_idx)) + '_demoframe2node_idx.json', 'w') as outfile:
         json.dump(demoframe2node_idx, outfile)
 
+def process_chunk(data):
+    # Load the data for this chunk.
+    recordings, demo_idcs_chunk = data
+
+    for demo_idx in demo_idcs_chunk:
+        create_single_demo_graph(recordings, demo_idx)
+
+
 if __name__ == "__main__":
-    recordings = utils.get_recordings(file_prefix="demo")
-    create_graph_data(recordings, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-    # create_graph_data(recordings, [0, 1])
+
+    num_cpus = 8
+
+    data_dir = "/tmp/flow_dataset"
+    task = "shape_sorting"
+    object_selected = "oval"  # trapeze, oval, semicircle
+    task_variant = "rP"  # rotation plus (+-pi)
+
+    export_dir = "/tmp/flow_dataset_contrastive/demo_" + task + "_" + object_selected + "_" + task_variant
+    if not os.path.exists(export_dir):
+        os.makedirs(export_dir)
+
+    recordings = utils.get_demonstrations(data_dir=data_dir,
+                                          num_episodes=1000,
+                                          task=task,
+                                          object_selected=object_selected,
+                                          task_variant=task_variant,
+                                          file_prefix="demo")
+
+    # chunking of demo=seed indices
+    demo_idcs = list(range(0, len(recordings)))
+    chunk_size = int(np.ceil(len(recordings) / num_cpus))
+    demo_idx_chunks = list(utils.chunks(demo_idcs, chunk_size))
+
+    chunk_data = list()
+    for idx_chunk in demo_idx_chunks:
+        chunk_data.append((recordings, idx_chunk))
+
+    ray.init(num_cpus=num_cpus,
+             include_dashboard=False,
+             _system_config={"automatic_object_spilling_enabled": True,
+                             "object_spilling_config": json.dumps(
+                                 {"type": "filesystem", "params": {"directory_path": "/tmp/spill"}}, )}, )
+
+    pool = Pool()
+    pool.map(process_chunk, [data for data in chunk_data])
--- flow_control/graph/utils.py
+++ flow_control/graph/utils.py
@@ -7,18 +7,26 @@ from glob import glob
 import numpy as np
 import matplotlib.pyplot as plt
 
-root_dir = "/tmp/flow_dataset"
-task = "shape_sorting"
-object_selected = "oval" # trapeze, oval, semicricle
-task_variant = "rP"  # rotation plus (+-pi)
 
 
-def get_configurations(root_dir=root_dir, num_episodes=20, object_selected=object_selected, prefix=""):
+def chunks(lst: object, n: object) -> object:
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+def get_configurations(root_dir, object_selected, task_variant, task, prefix, num_episodes=20):
     os.makedirs(root_dir, exist_ok=True)
     save_dir_template = os.path.join(root_dir, f"{prefix}_{task}_{object_selected}")
     for seed in range(num_episodes):
         save_dir = save_dir_template + f"_{task_variant}"+f"_seed{seed:03d}"
         yield object_selected, seed, save_dir
+
+def get_demonstrations(data_dir, object_selected, task_variant, task, file_prefix: str = "demo", num_episodes: int = 20):
+    demo_cfgs = get_configurations(root_dir=data_dir, prefix=file_prefix, num_episodes=num_episodes, object_selected=object_selected, task_variant=task_variant, task=task)
+    recordings = list()
+    for _, demo_seed, demo_dir in demo_cfgs:
+        recordings.append(demo_dir)
+    return recordings
 
 
 # Some helper functions to load the data from numpy format
@@ -44,14 +52,17 @@ def get_info(demo_dir, frame_index):
     return arr["info"].item()
 
 
-def get_recordings(file_prefix: str = "demo"):
-    demo_cfgs = get_configurations(prefix=file_prefix)
-    recordings = list()
-    for _, demo_seed, demo_dir in demo_cfgs:
-        recordings.append(demo_dir)
-    return recordings
+
 
 if __name__ == "__main__":
+    data_dir = "/tmp/flow_dataset"
+    task = "shape_sorting"
+    object_selected = "oval"  # trapeze, oval, semicricle
+    task_variant = "rP"  # rotation plus (+-pi)
+
+    export_dir = "/tmp/flow_dataset_contrastive/"
+
+
     demo_cfgs = get_configurations(prefix="demo")
     recordings = []
     for _, demo_seed, demo_dir in demo_cfgs:
