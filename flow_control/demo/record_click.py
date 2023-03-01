802022e20af27d702bacff612496d59443504667
Max Argus <argus.max@gmail.com>
2023-03-01 16:59:42 +0100

    fix record_click for real robot

--- flow_control/demo/record_click.py
+++ flow_control/demo/record_click.py
@@ -1,15 +1,19 @@
 """Script to record demos by click on camera images."""
 import math
-import platform
 import pickle
+import logging
+import platform
 
 import hydra
 import pygame
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
+from robot_io.actions.actions import Action
 from robot_io.robot_interface.base_robot_interface import GripperInterface as GI
-from gym_grasping.envs.robot_sim_env import RobotSimEnv
+
+# A logger for this file
+log = logging.getLogger(__name__)
 
 
 def get_point_in_world_frame(cam, t_tcp_cam, t_world_tcp, depth, clicked_point):
@@ -47,7 +51,8 @@ class ClickToPos:
         self.change_mode(0)
 
         pygame.init()
-        self.screen = pygame.display.set_mode((cam.width, cam.height))
+        cam_intrinsics = cam.get_intrinsics()
+        self.screen = pygame.display.set_mode((cam_intrinsics["width"], cam_intrinsics["height"]))
         self.screen.fill((255, 255, 255))
 
     def change_mode(self, new_index=None):
@@ -80,37 +85,37 @@ class ClickToPos:
 
     def _handle_events(self):
         """handle click events. """
+        new_action = False
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     self.done = True
                     break
-
                 elif event.key == pygame.K_SPACE:
                     self.change_gripper()
-
                 else:
                     print("keydown", event.key)
-
             elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
-                self.change_xy(*pygame.mouse.get_pos()[::-1])
-
+                pos = pygame.mouse.get_pos()
+                self.change_xy(*pos[::-1])
+                new_action = True
             elif event.type == pygame.MOUSEBUTTONUP and event.button == 2:
                 self.change_gripper()
-
+                new_action = True
             elif event.type == pygame.MOUSEBUTTONUP and event.button == 3:
                 self.change_mode()
-
+                new_action = True
             elif event.type == pygame.MOUSEBUTTONUP and event.button == 4:
                 self.mode_function(up=True)
-
+                new_action = True
             elif event.type == pygame.MOUSEBUTTONUP and event.button == 5:
                 self.mode_function(up=False)
-
+                new_action = True
             else:
                 #print("unknown", event)
                 pass
 
+        return new_action
 
     def change_gripper(self):
         """change the gripper state"""
@@ -120,7 +125,9 @@ class ClickToPos:
         """change the xy position"""
         depth_m = self.depth[x_pos, y_pos]
         point_world = get_point_in_world_frame(self.cam, self.t_tcp_cam, self.t_world_tcp, depth_m, (y_pos, x_pos))
-
+        if point_world is None:
+            log.info("Invalid motion, ignoring")
+            return
         new_action = [list(x) for x in self.last_action]
         new_action[0][0:2] = point_world[0:2] # change xy
         self.last_action = new_action
@@ -145,9 +152,14 @@ class ClickToPos:
         new_action[1] = (rot_change * old_rot).as_quat()
         self.last_action = new_action
 
-    def get_action(self):
+    def get_action(self, only_new_actions=True):
         """get actions"""
-        self._handle_events()
+        new_action = self._handle_events()
+        if only_new_actions:
+            if new_action:
+                return *self.last_action, self.gripper_state.value
+            return None
+
         return *self.last_action, self.gripper_state.value
 
     def save_io(self, rec_fn):
@@ -161,6 +173,10 @@ class ClickToPos:
         with open(io_fn, "wb") as f_obj:
             pickle.dump(self.io_log, f_obj)
 
+def vec2inst_act(action):
+    act = Action(target_pos=action[0], target_orn=action[1], gripper_action=action[2],
+                 ref="abs", path="lin", blocking=True)
+    return act
 
 def run_live_continous(cfg, env, initial_pose="neutral", clicked_points=None):
     """
@@ -169,25 +185,25 @@ def run_live_continous(cfg, env, initial_pose="neutral", clicked_points=None):
     """
     robot = env.robot
     cam = env.camera_manager.gripper_cam
-    assert cam.resize_resolution == [640, 480]  # don't crop like default panda teleop
+    #assert cam.resize_resolution == [640, 480]  # don't crop like default panda teleop
 
     # Step 1: move to neutral
     if initial_pose == "neutral":
         robot.move_to_neutral()
-        raise ValueError
-
-    env.robot.move_cart_pos_abs_lin(*initial_pose)
+        initial_pose = robot.get_tcp_pos_orn()
+    else:
+        robot.move_cart_pos(*initial_pose, ref="abs", blocking=True, path="lin")
 
     # Step 2: set up recorder
     recorder = hydra.utils.instantiate(cfg.recorder, env=env)
     recorder.recording = True
     action = None
-    obs, reward, done, e_info = env.step(action)
+    state, reward, done, e_info = env.step(action)
     info = {**e_info, "wp_name": "start"}
-    recorder.step(None, action, obs, reward, done, info)
+    recorder.step(state,None, None, reward, done, info)
 
     # Step 3: run waypoint recorder
-    t_tcp_cam = cam.get_extrinsic_calibration()
+    t_tcp_cam = cam.get_extrinsic_calibration(robot_name=robot.name)
     wp_factory = ClickToPos(cam, t_tcp_cam, initial_pose)
 
     while 1:
@@ -202,14 +218,15 @@ def run_live_continous(cfg, env, initial_pose="neutral", clicked_points=None):
 
         action = wp_factory.get_action()
         if action is not None:
-            print(action)
-            robot.move_cart_pos_abs_lin(action[0], action[1])
-            print(action[2])
+            robot.move_cart_pos(action[0], action[1], ref="abs", blocking=True, path="lin")
             if action[2] == -1:
                 robot.close_gripper(blocking=True)
             elif action[2] == 1:
                 robot.open_gripper(blocking=True)
-        obs, reward, done, e_info = env.step(None)
+
+            state, reward, done, e_info = env.step(None)
+            recorder.step(state, vec2inst_act(action), None, reward, done, e_info)
+
         if done:
             print("env done")
             break
@@ -219,25 +236,29 @@ def run_live_continous(cfg, env, initial_pose="neutral", clicked_points=None):
     return reward
 
 
-def test_pick_n_place(cfg):
+def test_simulation(cfg):
+    from gym_grasping.envs.robot_sim_env import RobotSimEnv
     new_pos = (-0.10, -0.60, 0.18)
     new_orn = tuple(R.from_euler("xyz", (math.pi, 0, math.pi/2)).as_quat())
-
     env = RobotSimEnv(task="pick_n_place", robot="kuka", renderer="debug", control="absolute",
                       img_size=(640, 480))
-
     reward = run_live_continous(cfg, env, initial_pose=(new_pos, new_orn))
-
     assert reward == 1.0
 
 
-@hydra.main(config_path="../../../robot_io/conf/", config_name="ur3_teleop.yaml")
+def test_robot(cfg):
+    robot = hydra.utils.instantiate(cfg.robot)
+    env = hydra.utils.instantiate(cfg.env, robot=robot)
+    run_live_continous(cfg, env, initial_pose="neutral")
+
+
+@hydra.main(config_path="/home/argusm/lang/robot_io/conf", config_name="ur3_teleop.yaml")
 def main(cfg=None):
-    # node = platform.uname().node
-    # if node in ('knoppers',):
-    #     robot = hydra.utils.instantiate(cfg.robot)
-    #     env = hydra.utils.instantiate(cfg.env, robot=robot)
-    test_pick_n_place(cfg)
+    node = platform.uname().node
+    if node in ('argusm-MS-7C76',):
+        test_robot(cfg)
+    else:
+        test_simulation(cfg)
 
 
 if __name__ == "__main__":
